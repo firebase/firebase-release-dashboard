a969f8d40840411b8db05cdb717bddf72a144537
forchane <daniellrq@outlook.com>
2023-07-26 17:26:01 -0500

    Remove scheduled releases button colours from text to button class

--- firebase-android-release-dashboard/src/components/ViewScheduledReleasesButton/styles.js
+++ firebase-android-release-dashboard/src/components/ViewScheduledReleasesButton/styles.js
@@ -2,6 +2,8 @@ import {makeStyles} from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   scheduledReleasesButton: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
     padding: theme.spacing(1),
   },
   scheduleIcon: {
@@ -10,8 +12,6 @@ const useStyles = makeStyles((theme) => ({
   },
   scheduledReleasesButtonText: {
     textTransform: "none",
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
     height: "24px",
   },
 }));
