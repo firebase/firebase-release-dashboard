adcb1fb1694553d34fc9e10fd67200ba6016fc12
David Motsonashvili <davidmotson@google.com>
2023-08-15 11:12:36 -0700

    update the urls now that functions are deployed

--- firebase-android-release-dashboard/src/api/constants.js
+++ firebase-android-release-dashboard/src/api/constants.js
@@ -1,5 +1,5 @@
-export const ADD_RELEASES_URL = "https://addReleases-TEMP.run.app/";
-export const DELETE_RELEASE_URL = "https://deleteRelease-TEMP.run.app/";
-export const REFRESH_RELEASE_URL = "https://refreshRelease-TEMP.run.app/";
-export const MODIFY_RELEASE_URL = "https://modifyRelease-TEMP.run.app/";
-export const GET_RELEASES_URL = "https://getReleases-TEMP.run.app/";
+export const ADD_RELEASES_URL = "https://addreleases-z5oaluwqtq-uc.a.run.app/";
+export const DELETE_RELEASE_URL = "https://deleterelease-z5oaluwqtq-uc.a.run.app/";
+export const REFRESH_RELEASE_URL = "https://refreshrelease-z5oaluwqtq-uc.a.run.app/";
+export const MODIFY_RELEASE_URL = "https://modifyrelease-z5oaluwqtq-uc.a.run.app/";
+export const GET_RELEASES_URL = "https://getreleases-z5oaluwqtq-uc.a.run.app/";
